c21d7962839b00e86643b2e7807bc71295b6e78e
Angelo Ashmore <angeloashmore@me.com>
2015-12-21 19:17:01 -1000

    Clean up config

--- config.rb
+++ config.rb
@@ -1,44 +1,36 @@
-###
-# Page options, layouts, aliases and proxies
-###
-
 # Per-page layout changes:
-#
-# With no layout
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
+# Helpers
+helpers do
+  def nav_link_to(link_text, url, options = {})
+    root = options.delete(:root)
+    is_active = (!root && current_page.url.start_with?(url)) ||
+                current_page.url == url
+    options[:class] ||= ''
+    options[:class] << '--is-active' if is_active
+    link_to(link_text, url, options)
+  end
+end
 
 # General configuration
+set :css_dir, 'assets/stylesheets'
+set :js_dir, 'assets/javascripts'
+set :images_dir, 'assets/images'
 
-# Reload the browser automatically whenever files change
+activate :directory_indexes
+
+# Development-specific configuration
 configure :development do
   activate :livereload
 end
 
-###
-# Helpers
-###
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
 # Build-specific configuration
 configure :build do
-  # Minify CSS on build
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_css
+  activate :minify_javascript
+  activate :asset_hash
+  activate :relative_assets
 end
