618ce7c98f32e05c374b9f010441d463b47096ca
Angelo Ashmore <angeloashmore@me.com>
2016-01-18 20:16:37 -1000

    Add subpages with styling

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
 gem 'middleman', '>= 4.0.0'
 gem 'middleman-livereload'
 gem 'middleman-syntax'
+gem 'middleman-blog'
 
 # Styling
 gem 'slim'
--- config.rb
+++ config.rb
@@ -2,7 +2,9 @@
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
-page '/learn/*', layout: 'content', data: { sidebar: 'learn/sidebar' }
+page '/', layout: 'layout'
+page '/learn/*', data: { sidebar: 'learn/sidebar' }
+page '/blog/*', data: { sidebar: 'blog/sidebar' }
 proxy '/learn/index.html', '/learn/introduction/index.html'
 
 # Helpers
@@ -19,17 +21,56 @@ helpers do
   def copyright
     "&copy; 2014-#{Time.now.year} Ruby Object Mapper"
   end
+
+  def authors_twitter_url(author)
+    config.authors_twitter_urls[author]
+  end
 end
 
 # General configuration
+set :layout, 'content'
 set :css_dir, 'assets/stylesheets'
 set :js_dir, 'assets/javascripts'
 set :markdown_engine, :redcarpet
 set :markdown, :tables => true, :autolink => true, :gh_blockcode => true,
     :fenced_code_blocks => true
+set :authors_twitter_urls, {
+  'Don Morrison' => 'https://twitter.com/elskwid',
+  'Piotr Solnica' => 'https://twitter.com/_solnic_',
+  'Mark Rickerby' => 'https://twitter.com/maetl'
+}
+set :projects, %w[
+  rom
+  rom-cassandra
+  rom-couchdb
+  rom-csv
+  rom-dm
+  rom-event_store
+  rom-git
+  rom-http
+  rom-influxdb
+  rom-json
+  rom-kafka
+  rom-lotus
+  rom-mongo
+  rom-neo4j
+  rom-rails
+  rom-redis
+  rom-rethinkdb
+  rom-roda
+  rom-sql
+  rom-yaml
+  rom-yesql
+]
 
-activate :directory_indexes
+activate :blog,
+  prefix: 'blog',
+  layout: 'blog_article',
+  permalink: '{title}.html',
+  paginate: true,
+  tag_template: 'blog/tag.html'
 activate :syntax, css_class: 'syntax'
+activate :directory_indexes
 activate :external_pipeline,
   name: :webpack,
   command: build? ? './node_modules/webpack/bin/webpack.js --bail' : './node_modules/webpack/bin/webpack.js --watch -d',
